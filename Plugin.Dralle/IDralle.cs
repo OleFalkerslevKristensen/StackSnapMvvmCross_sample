bf407ebb43b3c3e3dca848423626c2f2fbfe5826
OleFalkerslevKristensen <olefalkerslevkristensen@gmail.com>
2014-08-25 11:38:27 +0200

    -
    -

---
+++ Plugin.Dralle/IDralle.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Plugin.Dralle
+{
+    public interface IDralle
+    {
+        void ping(string serverId, string username, string password);
+    }
+}
---
+++ Plugin.Dralle/PluginLoader.cs
@@ -0,0 +1,26 @@
+﻿using Cirrious.CrossCore;
+using Cirrious.CrossCore.Plugins;
+using System;
+
+
+namespace Plugin.Dralle
+{
+    public class PluginLoader
+        : IMvxPluginLoader
+    {
+        public static readonly PluginLoader Instance = new PluginLoader();
+
+        public void EnsureLoaded()
+        {
+            var manager = Mvx.Resolve<IMvxPluginManager>();
+            try
+            {
+                manager.EnsurePlatformAdaptionLoaded<PluginLoader>();
+            }
+            catch (Exception exc)
+            {
+                string t = exc.Message;
+            }
+        }
+    }
+}
